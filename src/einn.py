ec5e368f9d5f39abdee2b7d40ae02ac593e91ba0
mundi55 <gfa4@hi.is>
2020-04-06 11:08:26 +0000

    fyrsta

--- src/einn.py
+++ src/einn.py
@@ -6,19 +6,17 @@ class einn:
     def __init__(self):
         self.status = h
         self.svaedi = 0
-        self.xhnit = 0
-        self.yhnit = 0
-        self.xhradi = 0
-        self.yhradi = 0
+        self.x = np.random.rand(n)
+        self.y = np.random.rand(n)
+        self.vx = speed * np.random.rand(n)
+        self.vy = speed * np.random.rand(n)
 
-    def hnit(self):
-        x = np.random.rand(n)
-        y = np.random.rand(n)
-
-    def hradi(self):
-        vx = speed * np.random.rand(n)
-        vy = speed * np.random.rand(n)
 
     def teikna(self):
-        #teikna puntk?
+        pygame.draw.circle(windowSurface, BLUE, \
+                           (int(600 * self.x), int(400 * self.y), 5, 0)
+
+
+    end
+
 
--- src/hermun.py
+++ src/hermun.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame
+import einn.py
 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -15,5 +16,24 @@ pygame.display.set_caption('Covid-19 hermir')
 
 FRAMES_PER_SECOND = 30
 fpsClock = pygame.time.Clock()
+n = 10
+while True:
+    for i in range(n):
+        # Reverse directon if point hits the boundary
+        if x[i] < 0 or x[i] > 1:
+            vx[i] = -1 * vx[i]
+        if y[i] < 0 or y[i] > 1:
+            vy[i] = -1 * vy[i]
+        x[i] += vx[i]
+        y[i] += vy[i]
 
-while
+
+    teikna()
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FRAMES_PER_SECOND)
