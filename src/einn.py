7ae58e50d912e6c4bef7e5c3b4f9b6e406e18868
mundi55 <gfa4@hi.is>
2020-04-06 14:47:55 +0000

    annað

--- src/einn.py
+++ src/einn.py
@@ -4,7 +4,7 @@ import numpy as np
 class einn:
 
     def __init__(self,stat):
-        self.status = stat #h,l,s
+        self.status = stat  #h,l,s
         self.svaedi = 0
         self.x = np.random.rand()
         self.y = np.random.rand()
@@ -12,21 +12,23 @@ class einn:
         self.vy = speed * np.random.rand()
         self.i = 0
 
-    def teikna(self):
+    def teikna(self, pygame, windowSurface):
         pygame.draw.circle(windowSurface, BLUE, \
                            (int(600 * self.x), int(400 * self.y), 5, 0))
 
 
     def astand(self,st):
-        if self.st == "s":
-            self.status = "s"
+        if self.st == 0:
+            self.status = 0
             return
-        if self.st == "l":
-            self.status = "l"
+        if self.st == 1:
+            self.status = 1
             return
-        if self.st == "h":
-            self.status = "h"
+        if self.st == 2:
+            self.status = 2
             return
+        if self.st == 3:
+            self.status = 2
 
 
     def teljari(self):
--- src/hermun.py
+++ src/hermun.py
@@ -17,21 +17,21 @@ pygame.display.set_caption('Covid-19 hermir')
 
 FRAMES_PER_SECOND = 30
 fpsClock = pygame.time.Clock()
-n = 10
-i=0
+
+for i =
+
 while True:
    # for i in range(n):
     #    # Reverse directon if point hits the boundary
      #   if x[i] < 0 or x[i] > 1:
       #      vx[i] = -1 * vx[i]
        # if y[i] < 0 or y[i] > 1:
-        #    vy[i] = -1 * vy[i]
-        #x[i] += vx[i]
-        #y[i] += vy[i]
+        #    vy   [i] = -1 * vy[i]
+   # x[i] += vx[i]
+   # y[i] += vy[i]
 
-    for i in range(n):
-        prufa = einn()
-        prufa.teikna()
+   for i in range(n):
+
 
 
     for event in pygame.event.get():
