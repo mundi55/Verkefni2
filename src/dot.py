01f2059804f590cdab131e7c26a2b65b1aa05d02
Sv1Thor <60746171+Sv1Thor@users.noreply.github.com>
2020-04-22 17:35:34 +0000

    Update dot.py

--- src/dot.py
+++ src/dot.py
@@ -4,11 +4,13 @@ import random
 import time
 
 class Dot:
+    # Colors to show different stages of infection
     BLUE = (0, 0, 255)
     LIGHT_RED = (255, 100, 100)
     RED = (255, 0, 0)
     GREEN = (100, 255, 0)
 
+    # Set up characteristics for each dot
     def __init__(self, n, speed, color=BLUE):
         self.x = random.random()
         self.y = random.random()
@@ -21,12 +23,14 @@ class Dot:
         self.vx = speed * random.random()
         self.vy = speed * random.random()
         self.color = color
-            
+
+    # Activated if dot becomes infected
     def infect(self, color=LIGHT_RED):
         self.status = 1
         self.cnt = time.time()
         self.color = color
 
+    # Reverse dots when they hit edges
     def edges(self):
         if self.x < 0 or self.x > 1:
             self.vx = -1 * self.vx
@@ -35,6 +39,7 @@ class Dot:
         self.x += self.vx
         self.y += self.vy
 
+    # Adds quarantine borders
     def reverse(self, xmax, ymax):
         if self.x < 0 or self.x > 1:
             self.vx = -1 * self.vx
@@ -47,6 +52,7 @@ class Dot:
         self.x += self.vx
         self.y += self.vy
 
+    # Adds holes to quarantine borders to create spillage
     def break_quarantine(self, xmax, ymax):
         if self.x < 0 or self.x > 1:
             self.vx = -1 * self.vx
@@ -69,18 +75,22 @@ class Dot:
         self.x += self.vx
         self.y += self.vy
 
+    # Draws the dot visually
     def draw(self, windowSurface, xmax, ymax, radius):
         pygame.draw.circle(windowSurface, self.color, \
                                (int(xmax * self.x), int(ymax * self.y)), radius, 0)
 
+    # Activated when infected dot becomes infectious
     def infectious(self, color=RED):
         self.status = 2
         self.color = color
 
+    # Activated when infected dot recovers
     def recovered(self, color=GREEN):
         self.status = 3
         self.color = color
 
+    # Implements social distancing
     def social_dist1(self):
         self.vx = -1 * self.vx
         self.x += self.vx
